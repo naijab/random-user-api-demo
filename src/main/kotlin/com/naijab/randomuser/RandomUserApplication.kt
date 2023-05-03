61e56312f8323671272db4daaea1623db526c883
Nattapon Pondongnok <nainatjab999@gmail.com>
2023-05-03 22:04:59 +0700

    init project

---
+++ src/main/kotlin/com/naijab/randomuser/RandomUserApplication.kt
@@ -0,0 +1,11 @@
+package com.naijab.randomuser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RandomuserApplication
+
+fun main(args: Array<String>) {
+	runApplication<RandomuserApplication>(*args) 
+}
---
+++ src/test/kotlin/com/naijab/randomuser/RandomuserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naijab.randomuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RandomuserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
